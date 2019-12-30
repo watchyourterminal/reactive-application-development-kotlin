73d7dbe96a22a4682566b85ce24ecd1317d89ba2
AlskaPark <alska1039@gmail.com>
2019-12-30 12:03:56 +0900

    Added coroutine suspend

---
+++ actor/src/main/kotlin/coroutine/composing_suspending_functions/3_LazilyStartedAsync.kt
@@ -0,0 +1,28 @@
+package coroutine.composing_suspending_functions
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+private suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
+
+private suspend fun doSomethingUsefulTwo(): Int {
+    delay(1000L) // pretend we are doing something useful here, too
+    return 29
+}
+
+fun main() = runBlocking {
+    val time = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
+        one.start()
+        two.start()
+        println("The answer is ${one.await() + two.await()}")
+    }
+    println("Completed in $time ms")
+}
